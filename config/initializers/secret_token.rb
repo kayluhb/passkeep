d2c07ccd4a082d53175e82e7c149e42c76922f20
kayluhb <cbrown@blenderbox.com>
2011-02-24 16:06:50 -0500

    default rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Passkeep::Application.config.secret_token = '8edbff4cd283c19a369efdb6751120f1976b5f601a50a36140e2eaf9b54de48478aaa25c6e0f64e0ad9d0434b8c7919a1c69e8c8aef87548fae1cd2928b8f58a'
