6ffc153f08c3a3797ce69f2e8d156df9bea7861c
Caleb Brown <cbrown@blenderbox.com>
2011-02-25 08:58:20 -0500

    added properties to users and added a factory

--- db/migrate/20110224211516_devise_create_users.rb
+++ db/migrate/20110224211516_devise_create_users.rb
@@ -5,19 +5,21 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.recoverable
       t.rememberable
       t.trackable
+      t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
+      t.token_authenticatable
 
       # t.confirmable
-      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-      # t.token_authenticatable
-
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
+      t.string :time_zone, :default => 'Eastern Time (US & Canada)', :null => false
 
       t.timestamps
     end
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
+    add_index :users, :unlock_token,         :unique => true
     # add_index :users, :confirmation_token,   :unique => true
-    # add_index :users, :unlock_token,         :unique => true
   end
 
   def self.down
