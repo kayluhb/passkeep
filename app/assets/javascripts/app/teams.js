2d6f474d291c76d3d934ad0abde5a6382121c0c5
kayluhb <cbrown@blenderbox.com>
2012-02-12 17:21:15 -0500

    adding the auto complete to the teams page

--- app/assets/javascripts/app/teams.js
+++ app/assets/javascripts/app/teams.js
@@ -4,10 +4,18 @@ var TEAMS = (function($) {
     // app.foo = function() {  };
     // Private functions
     function init() {
+        $.ui.autocomplete.prototype._renderItem = function(ul, item) {
+            var re = new RegExp("^" + this.term, "gm"),
+            t = item.name.replace(re, '<span class="highlight">' + this.term + '</span>');
+            return $('<li></li>')
+                .data('item.autocomplete', item)
+                .append('<a>' + t + '</a>')
+                .appendTo(ul);
+        };
         $('#team_search').autocomplete({ select:onTeamSelect, source:USER_SEARCH_URL });
     }
     function onTeamSelect(e, ui) {
-        console.log('on team select')
+        console.log('on team select', ui, ui.item);
     }
     // Call the init function on load
     $(init);
