d26d470a6f27d467f1e0a3323a574841e7aed854
Caleb Brown <cbrown@blenderbox.com>
2012-02-29 08:53:47 -0500

    added a simple password generator

--- app/assets/javascripts/app/entry.js
+++ app/assets/javascripts/app/entry.js
@@ -8,16 +8,30 @@ var ENTRY = (function($) {
         $('#entry_attachment').customFile({ element:'span', status:true, text: 'Choose Attachment' });
         $('#entry_url, #entry_title').on(evts, suggest);
         stops = _.union(APP.STOPS, $('#entry_tag_list').val().split(', '));
+        $('.icon-leaf').click(generate);
     }
+    function generate(e) {
+        var nums = '0123456789',
+        lowers = 'abcdefghijklmnopqrstuvwxyz',
+        uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        others = '~!@#$%^&*()-_=+[{]}\\|<>/? ',
+        chars = [rando(nums), rando(lowers), rando(uppers), rando(others), rando(nums), rando(lowers), rando(uppers), rando(others), rando(uppers), rando(others)];
+        $('#entry_password').val(_.shuffle(chars).join(''));
+    }
+    function rando(str) {
+        var num = Math.floor(Math.random() * str.length);
+		    return str.substring(num, num + 1);
+		}
     function suggest() {
         var suggestions = $('#entry_url').val().split('http://').join('').split('https://').join('').split('.');
         suggestions.push($et.val());
         //sugesstions = _.union(suggestions, $et.val().split(' '));
         suggestions = _.difference(suggestions, stops);
         suggestions = _.reject(suggestions, function(val){ return val.length < 3; });
-        console.log('suggest', _.uniq(suggestions));
+        //console.log('suggest', _.uniq(suggestions));
     }
     // Call the init function on load
     $(init);
     return app;
 } (jQuery));
+
