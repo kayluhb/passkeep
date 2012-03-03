a675ce93717c4b8ea946daec9afd12102e9a4d06
kayluhb <cbrown@blenderbox.com>
2012-03-03 15:22:09 -0500

    updating the forms

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,9 @@
 module UsersHelper
+
+  def last_sign_in user
+    user.current_sign_in_at.blank? ? "Never signed in" : l(user.current_sign_in_at)
+  end
+
   def user_edit_title user
     return "Edit #{user.full_name}" unless user.guid == current_user.guid
     "Edit your account"
